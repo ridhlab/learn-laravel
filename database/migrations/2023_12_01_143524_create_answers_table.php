a6b4cb497e11105290b445259c590939d88bba4d
ridhlab <mohridwan1707@gmail.com>
2023-12-01 21:43:47 +0700

    feat: add migration for table question and answer

---
+++ database/migrations/2023_12_01_143524_create_answers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('content', 200)->nullable(false);
+
+            $table->uuid('question_id')->nullable(false);
+
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
