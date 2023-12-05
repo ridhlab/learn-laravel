e7dd6f1fb1ca7457b5130d3b1d4dd591d8f88df6
ridhlab <mohridwan1707@gmail.com>
2023-12-05 11:02:30 +0700

    feat: conditional auth in sidebar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,22 +3,32 @@
     @include('components.notification')
     <div class="flex">
         <div class="w-48 h-[100vh] bg-slate-500 px-4 py-8 flex flex-col justify-between">
-            <div>
-                <div class="flex justify-center items-center mb-10">
+            <div class="flex flex-col gap-y-10">
+                <div class="flex justify-center items-center">
                     @include('components.logo')
                 </div>
-                <ul>
-                    <li class="text-white">
-                        <a href="/questions" class="hover:font-medium">
-                            Question
-                        </a>
-                    </li>
-                </ul>
+                <div class="flex flex-col gap-y-4">
+                    @if (Auth::check())
+                        <div class="flex flex-col items-center">
+                            <x-sui-user-male-circle class="w-10 h-10 text-white"/>
+                            <p class="text-white">{{Auth::user()->name}}</p>
+                        </div>
+                    @endif
+                    <ul>
+                        <li class="text-white">
+                            <a href="/questions" class="hover:font-medium">
+                                Question
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <form action="/auth/logout" method="POST">
-                @csrf
-                <button type="submit" class="text-white p-2 rounded-md border-2 border-white">Logout</button>
-            </form>
+            @if (Auth::check())
+                <form action="/auth/logout" method="POST">
+                    @csrf
+                    <button type="submit" class="text-white p-2 rounded-md border-2 border-white">Logout</button>
+                </form>
+            @endif
         </div>
         <div class="flex-1 p-8 bg-slate-200">
             @yield('mainContent')
